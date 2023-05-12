dfe36d1e1b3e47b2f1e39cbb64b0ca56f6f09a0a
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-05-12 12:23:29 +0200

    Updating form

--- src/lib/components/EmailCollector.svelte
+++ src/lib/components/EmailCollector.svelte
@@ -3,28 +3,28 @@
     let isSubmitting = false;
     let showSuccess = false;
     let showError = false;
+    let errorMessage = "";
 
     function handleSubmit(event: Event) {
-        event.preventDefault();
-
         const form = event.target as HTMLFormElement;
         const formData = new FormData(form);
 
         isSubmitting = true;
+        errorMessage = "";
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: new URLSearchParams(formData.toString())
+            body: new URLSearchParams(formData.toString()).toString()
         })
         .then(() => {
             form.reset();
             isSubmitting = false;
             showSuccess = true;
         })
-        .catch(() => {
-            form.reset();
+        .catch((error) => {
             isSubmitting = false;
             showError = true;
+            errorMessage = error;
         });
     }
 </script>
@@ -34,9 +34,11 @@
     class="flex flex-col gap-2 items-center"
     name="emailCollector"
     id="emailCollector"
-    data-netlify="true"
+    netlify
     netlify-honeypot="bot-field"
+    on:submit|preventDefault={handleSubmit}
 >
+    <input type="hidden" name="form-name" value="Email Collector" />
     <label class="hidden">
         Don't fill this out if you're human: <input name="bot-field" />
     </label>
@@ -47,7 +49,7 @@
 
     <div class="flex flex-col md:flex-row gap-2 items-center">
         <input name="email" id="email" type="text" placeholder="satoshin@gmx.com" class="w-72 py-1.5" />
-        <button on:submit={handleSubmit} type="submit" class="font-semibold font-josefin-sans-italic uppercase text-ltngYellow bg-ltngWhite/20 hover">Subscribe</button>
+        <button type="submit" class="font-semibold font-josefin-sans-italic uppercase text-ltngYellow bg-ltngWhite/20 hover">Subscribe</button>
     </div>
 
     {#if showSuccess}
@@ -55,6 +57,6 @@
     {/if}
 
     {#if showError}
-        <div class="text-red-500">Sorry, something went wrong. Try again.</div>
+        <div class="text-red-500">{errorMessage}</div>
     {/if}
 </form>
