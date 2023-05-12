76c560d6e2150a17f3d48f4046291a5d81a34e0f
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-05-12 15:37:28 +0200

    Refactor social icons

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
-    import TwitterIcon from '$lib/elements/icons/Twitter.svelte';
-    import TelegramIcon from '$lib/elements/icons/Telegram.svelte';
-    import YoutubeIcon from '$lib/elements/icons/Youtube.svelte';
-    import AngellistIcon from '$lib/elements/icons/Angellist.svelte';
     import EmailCollector from '$lib/components/EmailCollector.svelte';
+    import SocialIconLinks from '$lib/components/SocialIconLinks.svelte';
 </script>
 
 
@@ -13,35 +10,7 @@
     </div>
     <div class="flex flex-col md:flex-row gap-4 items-center justify-center text-sm">
         <div class="px-2">©️ 2023 Lightning Ventures</div>
-        <div class="flex flex-row gap-4 items-center justify-center">
-            <a href="https://twitter.com/ltngventures" class="w-5 h-5">
-                <TwitterIcon />
-            </a>
-            <a href="https://www.youtube.com/channel/UCFIf_EZD9MSMCJVvI5vnXAw" class="w-5 h-5">
-                <YoutubeIcon />
-            </a>
-            <a href="https://t.me/ltngventures" class="w-5 h-5">
-                <TelegramIcon />
-            </a>
-            <a
-                href="https://venture.angellist.com/lightningventures/syndicate?utm_campaign=syndicate_direct_link"
-                class="w-5 h-5"
-            >
-                <AngellistIcon />
-            </a>
-            <a
-                href="https://www.linkedin.com/company/lightning%E2%9A%A1%EF%B8%8Fventures"
-                class="w-5 h-5"
-            >
-                <img src="/images/linkedin.png" class="w-5 h-5" alt="LinkedIn" />
-            </a>
-            <a
-                href="nostr:nprofile1qqsr0hscuzxdcqww0nk3sz9jg8kqkjnfua2d2akwpcy0pnenwkas56cpzemhxue69uhhyetvv9ujumn0wd68ytnzv9hxgs8tehl"
-                class="w-5 h-5"
-            >
-                <img src="/images/nostr-white.webp" class="w-5 h-5" alt="Nostr" />
-            </a>
-        </div>
+        <SocialIconLinks />
     </div>
 
     <div class="flex flex-row divide-x items-center justify-center text-sm">
---
+++ src/lib/components/SocialIconLinks.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import TwitterIcon from '$lib/elements/icons/Twitter.svelte';
+    import TelegramIcon from '$lib/elements/icons/Telegram.svelte';
+    import YoutubeIcon from '$lib/elements/icons/Youtube.svelte';
+    import AngellistIcon from '$lib/elements/icons/Angellist.svelte';
+    import EmailCollector from '$lib/components/EmailCollector.svelte';
+    import EnvelopeIcon from '$lib/elements/icons/Envelope.svelte';
+</script>
+
+<div class="socialIconsContainer flex flex-row gap-4 items-center justify-center">
+    <a href="/contact" class="w-5 h-5">
+        <EnvelopeIcon klass="w-5 h-5"/>
+    </a>
+    <a href="https://twitter.com/ltngventures" class="w-5 h-5">
+        <TwitterIcon />
+    </a>
+    <a href="https://www.youtube.com/channel/UCFIf_EZD9MSMCJVvI5vnXAw" class="w-5 h-5">
+        <YoutubeIcon />
+    </a>
+    <a href="https://t.me/ltngventures" class="w-5 h-5">
+        <TelegramIcon />
+    </a>
+    <a
+        href="https://venture.angellist.com/lightningventures/syndicate?utm_campaign=syndicate_direct_link"
+        class="w-5 h-5"
+    >
+        <AngellistIcon />
+    </a>
+    <a
+        href="https://www.linkedin.com/company/lightning%E2%9A%A1%EF%B8%8Fventures"
+        class="w-5 h-5"
+    >
+        <img src="/images/linkedin.png" class="w-5 h-5" alt="LinkedIn" />
+    </a>
+    <a
+        href="nostr:nprofile1qqsr0hscuzxdcqww0nk3sz9jg8kqkjnfua2d2akwpcy0pnenwkas56cpzemhxue69uhhyetvv9ujumn0wd68ytnzv9hxgs8tehl"
+        class="w-5 h-5"
+    >
+        <img src="/images/nostr-white.webp" class="w-5 h-5" alt="Nostr" />
+    </a>
+</div>
