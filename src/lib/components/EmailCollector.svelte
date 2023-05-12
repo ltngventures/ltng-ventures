49b32b2c245c617a0d0799181cfb2007bdcb62cc
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-05-12 12:32:37 +0200

    Another form update

--- src/lib/components/EmailCollector.svelte
+++ src/lib/components/EmailCollector.svelte
@@ -32,8 +32,7 @@
 <form
     method="POST"
     class="flex flex-col gap-2 items-center"
-    name="emailCollector"
-    id="emailCollector"
+    name="Email Collector"
     netlify
     netlify-honeypot="bot-field"
     on:submit|preventDefault={handleSubmit}
@@ -49,7 +48,7 @@
 
     <div class="flex flex-col md:flex-row gap-2 items-center">
         <input name="email" id="email" type="text" placeholder="satoshin@gmx.com" class="w-72 py-1.5" />
-        <button type="submit" class="font-semibold font-josefin-sans-italic uppercase text-ltngYellow bg-ltngWhite/20 hover">Subscribe</button>
+        <button id="emailSubmitButton" type="submit" class="font-semibold font-josefin-sans-italic uppercase text-ltngYellow bg-ltngWhite/20 hover">Subscribe</button>
     </div>
 
     {#if showSuccess}
