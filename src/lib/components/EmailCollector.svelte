d5536b4d038227a4ea6b6bbe36c538daa89339a0
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-05-12 12:15:15 +0200

    Update email collector

--- src/lib/components/EmailCollector.svelte
+++ src/lib/components/EmailCollector.svelte
@@ -1,4 +1,32 @@
 <script lang="ts">
+
+    let isSubmitting = false;
+    let showSuccess = false;
+    let showError = false;
+
+    function handleSubmit(event: Event) {
+        event.preventDefault();
+
+        const form = event.target as HTMLFormElement;
+        const formData = new FormData(form);
+
+        isSubmitting = true;
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData.toString())
+        })
+        .then(() => {
+            form.reset();
+            isSubmitting = false;
+            showSuccess = true;
+        })
+        .catch(() => {
+            form.reset();
+            isSubmitting = false;
+            showError = true;
+        });
+    }
 </script>
 
 <form
@@ -6,7 +34,6 @@
     class="flex flex-col gap-2 items-center"
     name="emailCollector"
     id="emailCollector"
-    action="/success"
     data-netlify="true"
     netlify-honeypot="bot-field"
 >
@@ -20,6 +47,14 @@
 
     <div class="flex flex-col md:flex-row gap-2 items-center">
         <input name="email" id="email" type="text" placeholder="satoshin@gmx.com" class="w-72 py-1.5" />
-        <button type="submit" class="font-semibold font-josefin-sans-italic uppercase text-ltngYellow bg-ltngWhite/20 hover">Subscribe</button>
+        <button on:submit={handleSubmit} type="submit" class="font-semibold font-josefin-sans-italic uppercase text-ltngYellow bg-ltngWhite/20 hover">Subscribe</button>
     </div>
+
+    {#if showSuccess}
+        <div class="text-green-500">Thanks! You're now subscribed.</div>
+    {/if}
+
+    {#if showError}
+        <div class="text-red-500">Sorry, something went wrong. Try again.</div>
+    {/if}
 </form>
