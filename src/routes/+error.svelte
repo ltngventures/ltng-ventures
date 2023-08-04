4bce9d20a4afe17f9ccfde434ca34201d1f477d1
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-08-04 17:14:47 +0200

    Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+</script>
+
+<div class="mx-auto my-8 text-center">
+    <h1 class="text-3xl font-bold">{$page.status}</h1>
+    <h2 class="text-xl">{$page.error?.message}</h2>
+</div>
