bea5ee9e8434848118348d76ecc0db85f61b3d69
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-10-27 08:18:10 +0200

    Update events

--- src/lib/data/events.ts
+++ src/lib/data/events.ts
@@ -1,23 +1,5 @@
 export const events: App.Event[] = [
     {
-        title: "TABConf",
-        date: "Sept 6-9 2023",
-        location: "Atlanta, Georgia",
-        url: "https://2023.tabconf.com/",
-    },
-    {
-        title: "Pacific Bitcoin",
-        date: "Oct 5-6 2023",
-        location: "Los Angeles, California",
-        url: "https://www.pacificbitcoin.com/",
-    },
-    {
-        title: "Bitcoin Amsterdam",
-        date: "Oct 12-13 2023",
-        location: "Amsterdam, The Netherlands",
-        url: "https://b.tc/conference/amsterdam",
-    },
-    {
         title: "NostrAsia",
         date: "Nov 1-4 2023",
         location: "Tokyo, Japan",
@@ -42,6 +24,12 @@ export const events: App.Event[] = [
         url: "https://www.bitcoinfreedomfestival.com",
     },
     {
+        title: "BitcoinDay",
+        date: "Jan 20 2024",
+        location: "Naples Florida",
+        url: "https://bitcoinday.io/",
+    },
+    {
         title: "Adopting Bitcoin (Cape Town)",
         date: "Jan 26-28 2024",
         location: "Cape Town, South Africa",
@@ -52,5 +40,17 @@ export const events: App.Event[] = [
         date: "Mar 1-3 2024",
         location: "Maderia Island, Portugal",
         url: "https://bitcoinatlantis.com/",
+    },
+    {
+        title: "BitBlockBoom",
+        date: "Apr 11-15 2024",
+        location: "Dallas, Texas",
+        url: "https://bitblockboom.com/?coupon=ltngventures",
+    },
+    {
+        title: "BTC Prague",
+        date: "June 13-15 2024",
+        location: "Prague, Czech Republic",
+        url: "https://www.btcprague.com/",
     },
 ];
