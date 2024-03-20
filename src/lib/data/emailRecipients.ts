aa349c1f08ebc7ea581c052c3074ab06b45e99c6
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2024-03-20 14:57:20 +0100

    Update email addresses

--- src/lib/data/emailRecipients.ts
+++ src/lib/data/emailRecipients.ts
@@ -1,14 +1,3 @@
-export const EMAIL_RECIPIENTS_MINING = [
-    "mike@ltng.ventures",
-    "seb@ltng.ventures",
-    "vivek@ltng.ventures",
-    "badgercomp@protonmail.com",
-    "lee@ltng.ventures",
-];
+export const EMAIL_RECIPIENTS_MINING = ["hello@ltng.ventures"];
 
-export const EMAIL_RECIPIENTS_GENERAL = [
-    "mike@ltng.ventures",
-    "vivek@ltng.ventures",
-    "seb@ltng.ventures",
-    "lee@ltng.ventures",
-];
+export const EMAIL_RECIPIENTS_GENERAL = ["hello@ltng.ventures"];
