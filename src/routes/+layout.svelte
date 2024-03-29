8c2d17cc9efe05327a3f2b800553a78514da7baf
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2024-03-29 14:22:45 +0100

    Ensure anchor scroll-to works

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,16 @@
 <script>
-    import '../app.css';
-    import Footer from '$lib/components/Footer.svelte';
-    import Header from '$lib/components/Header.svelte';
-    import Hero from '$lib/components/Hero.svelte';
-    // import BoltIcon from '$lib/elements/icons/Bolt.svelte';
-    import { page } from '$app/stores';
+    import "../app.css";
+    import Footer from "$lib/components/Footer.svelte";
+    import Header from "$lib/components/Header.svelte";
+    import Hero from "$lib/components/Hero.svelte";
+    import { onMount } from "svelte";
+    import { page } from "$app/stores";
+
+    onMount(() => {
+        const { hash } = document.location;
+        const scrollTo = hash && document.getElementById(hash.slice(1));
+        if (scrollTo) scrollTo.scrollIntoView();
+    });
 </script>
 
 <!-- {#if $page.url.pathname !== '/'}
