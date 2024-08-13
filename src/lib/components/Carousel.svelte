ebf2044170c87e54552641f3b169bc529677b9c2
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2024-08-13 17:09:47 +0200

    Update dependencies and fix scrollbars on homepage

--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
     export let testimonials: App.Testimonial[];
-    export let direction: string = 'Right';
+    export let direction: string = "Right";
 
     const klass = `testimonials${direction}`;
 </script>
 
-
-<div class="carousel flex flex-nowrap flex-row items-center overflow-x-scroll w-screen">
+<div class="carousel flex flex-nowrap flex-row items-center overflow-hidden w-screen">
     <div class="{klass} flex flex-nowrap flex-row gap-32 items-center">
         {#each testimonials as testimonial, i}
-            <div class="flex shrink-0 grow-0 flex-row gap-6 items-center ">
-                <img src={testimonial.img} alt="testimonial{i}" class="w-24 md:w-40 h-24 md:h-40 rounded-full shadow-lg" />
+            <div class="flex shrink-0 grow-0 flex-row gap-6 items-center">
+                <img
+                    src={testimonial.img}
+                    alt="testimonial{i}"
+                    class="w-24 md:w-40 h-24 md:h-40 rounded-full shadow-lg"
+                />
                 <div class="prose dark:prose-invert md:prose-lg w-64 md:w-96">
                     <p>&ldquo;{testimonial.blurb}&rdquo;</p>
                     <p class="text-base">&ndash; {testimonial.nameAndRole}</p>
@@ -19,7 +22,11 @@
         {/each}
         {#each testimonials as testimonial, i}
             <div class="flex shrink-0 grow-0 flex-row gap-6 items-center">
-                <img src={testimonial.img} alt="testimonial{i}" class="w-24 md:w-40 h-24 md:h-40 rounded-full shadow-lg" />
+                <img
+                    src={testimonial.img}
+                    alt="testimonial{i}"
+                    class="w-24 md:w-40 h-24 md:h-40 rounded-full shadow-lg"
+                />
                 <div class="prose dark:prose-invert md:prose-lg w-64 md:w-96">
                     <p>&ldquo;{testimonial.blurb}&rdquo;</p>
                     <p class="text-base">&ndash; {testimonial.nameAndRole}</p>
