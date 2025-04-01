a4448b0eb51a1ec19522d3730f3e6995e9306aa9
rockstardev <5191402+rockstardev@users.noreply.github.com>
2025-04-01 13:48:54 -0500

    Update events.ts

--- src/lib/data/events.ts
+++ src/lib/data/events.ts
@@ -1,69 +1,183 @@
 export const events: App.Event[] = [
     {
-        title: "Bitcoin Medellin",
-        date: "Jan 17-18 2025",
-        location: "Medellin, Colombia",
-        url: "https://bitcoinmedellin.com",
-    },
-    {
-        title: "Adopting Bitcoin, Cape Town",
-        date: "Jan 24-25 2025",
-        location: "Cape Town, South Africa",
-        url: "https://za25.adoptingbitcoin.org",
-    },
-    {
-        title: "Plan ₿ Forum El Salvador",
-        date: "Jan 30-31 2025",
-        location: "San Salvador, El Salvador",
-        url: "https://planb.sv",
-    },
-    {
-        title: "SATS’N’FACTS",
-        date: "Feb 8-10 2025",
-        location: "Chiang Mai, Thailand",
-        url: "https://satsandfacts.btc.pub",
-    },
-    {
         title: "Bitcoin Freedom Festival",
-        date: "Fed 13-15 2025",
+        date: "Feb 13-15 2025",
         location: "Uvita, Costa Rica",
-        url: "https://www.bitcoinfreedomfestival.com",
+        url: "https://www.bitcoinfreedomfestival.com/"
     },
     {
-        title: "bitcoin++ floripa",
+        title: "bitcoin++ catches some waves",
         date: "Feb 19-22 2025",
-        location: "Florianopolis, Brasil",
-        url: "https://btcplusplus.dev/conf/floripa",
+        location: "Florianópolis, Brazil",
+        url: "https://btcplusplus.dev/conf/floripa"
+    },
+    {
+        title: "Bitcoin Investor Week",
+        date: "Feb 25-28 2025",
+        location: "New York City, NY, USA",
+        url: "https://lu.ma/bitcoininvestorweek"
+    },
+    {
+        title: "Bitcoin Renaissance",
+        date: "Feb 27-28 2025",
+        location: "Denver, CO, USA",
+        url: "https://bitcoin-renaissance.com/"
+    },
+    {
+        title: "Bitcoin Ski Summit",
+        date: "Mar 5-9 2025",
+        location: "Teton Village, WY, USA",
+        url: "https://www.apres.tech/bitcoin-ski-summit-2024"
     },
     {
         title: "Bitcoin Alive",
         date: "Mar 8 2025",
         location: "Sydney, Australia",
-        url: "https://bitcoinalive.io/",
+        url: "https://bitcoinalive.io/"
     },
     {
-        title: "BitBlockBoom",
+        title: "BTC Vortex",
+        date: "Mar 9-16 2025",
+        location: "Miami, FL, USA",
+        url: "https://btcvortex.com/"
+    },
+    {
+        title: "Bitcoin Takeover",
+        date: "Mar 14 2025",
+        location: "Austin, TX, USA",
+        url: "https://bitcointakeover.org/"
+    },
+    {
+        title: "Bush Bash",
+        date: "Mar 28-30 2025",
+        location: "Beechworth, Australia",
+        url: "https://bitcoinbushbash.info/"
+    },
+    {
+        title: "Opt Out Conf",
+        date: "Mar 29 2025",
+        location: "Buenos Aires, Argentina",
+        url: "https://optoutconf.com/"
+    },
+    {
+        title: "BitBlockBoom!",
         date: "Apr 3-6 2025",
-        location: "Dallas, Texas",
-        url: "https://bitblockboom.com",
+        location: "Dallas, TX, USA",
+        url: "https://bitblockboom.com/"
     },
     {
-        title: "bitcoin++ austin",
+        title: "MIT Bitcoin Expo",
+        date: "Apr 5-6 2025",
+        location: "Cambridge, MA, USA",
+        url: "https://mitbtcexpo.org/"
+    },
+    {
+        title: "Bitcoin Strategic Reserve Summit",
+        date: "Apr 15 2025",
+        location: "Online",
+        url: "https://bitcoinreservesummit.com/"
+    },
+    {
+        title: "OPNEXT",
+        date: "Apr 11-12 2025",
+        location: "Tysons, VA, USA",
+        url: "https://opnext.dev/"
+    },
+    {
+        title: "CheatCode Conference",
+        date: "Apr 11-13 2025",
+        location: "Bedford, UK",
+        url: "https://www.cheatcode.co.uk/"
+    },
+    {
+        title: "Swiss Bitcoin Conference",
+        date: "Apr 24-27 2025",
+        location: "Kreuzlingen, Switzerland",
+        url: "https://swiss-bitcoin-conference.com/"
+    },
+    {
+        title: "Bitcoin Oasis",
+        date: "Apr 28-29 2025",
+        location: "Dubai, UAE",
+        url: "https://www.bitcoin-oasis.com/"
+    },
+    {
+        title: "Bitcoin for Corporations [MicroStrategy World]",
+        date: "May 6-7 2025",
+        location: "Las Vegas, NV, USA",
+        url: "https://www.microstrategy.com/en/resources/events/world-2025/bitcoin-for-corporations"
+    },
+    {
+        title: "Tuscany Lightning Summit",
+        date: "May 7-8 2025",
+        location: "Viareggio, Italy",
+        url: "https://tuscanysummit.com/"
+    },
+    {
+        title: "bitcoin++ dives deep",
         date: "May 7-9 2025",
-        location: "Austin, Texas",
-        url: "https://btcplusplus.dev/conf/atx25",
+        location: "Austin, TX, USA",
+        url: "https://btcplusplus.dev/conf/atx25"
     },
     {
-        title: "Adopting Bitcoin Arnhem",
-        date: "May 25 2025",
-        location: "Arnhem, Netherlands",
-        url: "https://www.adoptingbitcoinarnhem.com",
+        title: "BitCare Forum",
+        date: "May 10 2025",
+        location: "Brescia, Italy",
+        url: "https://bitcareforum.it/"
     },
     {
-        title: "Bitcoin 2025",
+        title: "Pizza Day Prague",
+        date: "May 17-18 2025",
+        location: "Prague, Czech Republic",
+        url: "https://pizzaday.cz/"
+    },
+    {
+        title: "Bitcoin Filmfest 2025",
+        date: "May 22-25 2025",
+        location: "Warsaw, Poland",
+        url: "https://bitcoinfilmfest.com/"
+    },
+    {
+        title: "Bitcoin Ireland Conference",
+        date: "May 24 2025",
+        location: "Dublin, Ireland",
+        url: "https://www.bitcoinireland.eu/"
+    },
+    {
+        title: "Oslo Freedom Forum",
+        date: "May 26-28 2025",
+        location: "Oslo, Norway",
+        url: "https://oslofreedomforum.com/"
+    },
+    {
+        title: "Bitcoin 2025 Conference (BTC Inc.)",
         date: "May 27-29 2025",
-        location: "Las Vegas, Nevada",
-        url: "https://b.tc/conference/2025",
+        location: "Las Vegas, NV, USA",
+        url: "https://b.tc/conference/2025"
+    },
+    {
+        title: "Bitcoin Economic Forum",
+        date: "May 30-Jun 1 2025",
+        location: "Bretagne, France",
+        url: "https://www.breizhbitcoin.com/bitcoin-economic-forum/"
+    },
+    {
+        title: "Bitcoin Seoul Conference",
+        date: "Jun 4-6 2025",
+        location: "Seoul, South Korea",
+        url: "http://bitcoinseoul.kr/"
+    },
+    {
+        title: "Bitcoin Day Tampa",
+        date: "Jun 14 2025",
+        location: "Tampa, FL, USA",
+        url: "https://bitcoinday.io/"
+    },
+    {
+        title: "Sound Money Soiree",
+        date: "Jun 14 2025",
+        location: "Tampa, FL, USA",
+        url: "https://www.bitcoinbay.live/sound-money-soiree"
     },
     {
         title: "BTC Prague 2025",
